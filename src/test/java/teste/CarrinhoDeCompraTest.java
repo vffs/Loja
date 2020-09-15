1d59c42bc127b51271ab73614ddf05a2ffd276d8
vffs <vffs@a.recife.ifpe.edu.br>
2020-09-15 14:13:32 -0300

    teste deveRetornarMaiorValorSeCarrinhoContemMuitosElementos

--- src/test/java/teste/CarrinhoDeCompraTest.java
+++ src/test/java/teste/CarrinhoDeCompraTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
  *
  * @author valeria
  */
-
 public class CarrinhoDeCompraTest {
 
     @Test
@@ -21,9 +20,21 @@ public class CarrinhoDeCompraTest {
     @Test
     public void deveRetornarValorDoItemSeCarrinhoComUmElemento() {
         CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
-        
+
         carrinho.adiciona(new Item("Geladeira", 1, 900.0));
-        
+
         assertEquals(900.0, carrinho.maiorValor(), 0.0001);
     }
+
+    @Test
+    public void deveRetornarMaiorValorSeCarrinhoContemMuitosElementos() {
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+        
+        carrinho.adiciona(new Item("Geladeira", 1, 900.0));
+        carrinho.adiciona(new Item("Fogão", 1, 1500.0));
+        carrinho.adiciona(new Item("Máquina de Lavar", 1, 750.0));
+        
+        assertEquals(1500.0, carrinho.maiorValor(), 0.0001);
+    }
+    
 }
