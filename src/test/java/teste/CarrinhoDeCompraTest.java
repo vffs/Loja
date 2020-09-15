e4c6aa55e0e42d550c8eb42c0b311668d3bb5367
vffs <vffs@a.recife.ifpe.edu.br>
2020-09-15 14:05:50 -0300

    teste deveRetornarValorDoItemSeCarrinhoComUmElemento

--- src/test/java/teste/CarrinhoDeCompraTest.java
+++ src/test/java/teste/CarrinhoDeCompraTest.java
@@ -1,6 +1,7 @@
 package teste;
 
 import loja.CarrinhoDeCompras;
+import loja.Item;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
@@ -8,11 +9,21 @@ import org.junit.Test;
  *
  * @author valeria
  */
+
 public class CarrinhoDeCompraTest {
 
     @Test
     public void deveRetornarZeroSeCarrinhoVazio() {
         CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
         assertEquals(0.0, carrinho.maiorValor(), 0.0001);
+    }
+
+    @Test
+    public void deveRetornarValorDoItemSeCarrinhoComUmElemento() {
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+        
+        carrinho.adiciona(new Item("Geladeira", 1, 900.0));
+        
+        assertEquals(900.0, carrinho.maiorValor(), 0.0001);
     }
 }
