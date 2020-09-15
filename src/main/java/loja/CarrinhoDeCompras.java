04420836b58e07411f623126fc9db3f4d9a3701f
vffs <vffs@a.recife.ifpe.edu.br>
2020-09-15 13:52:00 -0300

    initial comit

---
+++ src/main/java/loja/CarrinhoDeCompras.java
@@ -0,0 +1,28 @@
+package loja;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author valeria
+ */
+
+public class CarrinhoDeCompras {
+
+    private List<Item> itens;
+
+    public CarrinhoDeCompras() {
+        this.itens = new ArrayList<Item>();
+    }
+
+    public void adiciona(Item item) {
+        this.itens.add(item);
+    }
+
+    public List<Item> getItens() {
+        return Collections.unmodifiableList(itens);
+    }
+
+}
