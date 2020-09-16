9a4bc026ee306028723534abcaeca01a5e63d20d
vffs <vffs@a.recife.ifpe.edu.br>
2020-09-16 12:54:17 -0300

    retirado código repetido, criado um metodo inicializa @Before

--- src/test/java/teste/CarrinhoDeCompraTest.java
+++ src/test/java/teste/CarrinhoDeCompraTest.java
@@ -3,6 +3,7 @@ package teste;
 import loja.CarrinhoDeCompras;
 import loja.Item;
 import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -10,31 +11,34 @@ import org.junit.Test;
  * @author valeria
  */
 public class CarrinhoDeCompraTest {
+    CarrinhoDeCompras carrinho;
+    
+    @Before
+    public void inicializa(){
+        this.carrinho = new CarrinhoDeCompras();
+    }
+    
 
     @Test
-    public void deveRetornarZeroSeCarrinhoVazio() {
-        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+    public void deveRetornarZeroSeCarrinhoVazio() {     
         assertEquals(0.0, carrinho.maiorValor(), 0.0001);
     }
 
     @Test
     public void deveRetornarValorDoItemSeCarrinhoComUmElemento() {
-        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+        
+        this.carrinho.adiciona(new Item("Geladeira", 1, 900.0));
 
-        carrinho.adiciona(new Item("Geladeira", 1, 900.0));
-
-        assertEquals(900.0, carrinho.maiorValor(), 0.0001);
+        assertEquals(900.0, this.carrinho.maiorValor(), 0.0001);
     }
 
     @Test
-    public void deveRetornarMaiorValorSeCarrinhoContemMuitosElementos() {
-        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+    public void deveRetornarMaiorValorSeCarrinhoContemMuitosElementos() {    
+        this.carrinho.adiciona(new Item("Geladeira", 1, 900.0));
+        this.carrinho.adiciona(new Item("Fogão", 1, 1500.0));
+        this.carrinho.adiciona(new Item("Máquina de Lavar", 1, 750.0));
         
-        carrinho.adiciona(new Item("Geladeira", 1, 900.0));
-        carrinho.adiciona(new Item("Fogão", 1, 1500.0));
-        carrinho.adiciona(new Item("Máquina de Lavar", 1, 750.0));
-        
-        assertEquals(1500.0, carrinho.maiorValor(), 0.0001);
+        assertEquals(1500.0, this.carrinho.maiorValor(), 0.0001);
     }
     
 }
